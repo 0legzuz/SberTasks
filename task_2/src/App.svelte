c5b45ffb18be33e88a80c421d71296de05304558
0legzuz <olegzuz.olegzuz@gmail.com>
2024-05-01 18:07:00 +0300

    add task2

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,141 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from "svelte";
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  let currencies = [];
+  let baseCurrency = "USD";
+  let targetCurrency = "RUB";
+  let baseAmount: number = 1;
+  let targetAmount = null;
+  let exchangeRate = null;
+  let isLoading = false;
+
+  async function fetchData() {
+    isLoading = true;
+    try {
+      const response = await fetch(
+        `https://open.er-api.com/v6/latest/${baseCurrency}`
+      );
+      const data = await response.json();
+      currencies = Object.keys(data.rates);
+      exchangeRate = data.rates[targetCurrency];
+      updateTargetAmount();
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      isLoading = false;
+    }
+  }
+
+  onMount(fetchData);
+
+  function handleBaseCurrencyChange(event) {
+    baseCurrency = event.target.value;
+    fetchData();
+  }
+
+  function handleTargetCurrencyChange(event) {
+    targetCurrency = event.target.value;
+    fetchData();
+  }
+
+  function handleBaseAmountChange(event) {
+    baseAmount = event.target.value;
+    updateTargetAmount();
+  }
+
+  function handleTargetAmountChange(event) {
+  targetAmount = event.target.value;
+  baseAmount =
+    exchangeRate !== null
+      ? parseFloat(targetAmount) / exchangeRate
+      : null;
+}
+
+  function updateTargetAmount() {
+    targetAmount =
+      exchangeRate !== null ? (baseAmount * exchangeRate).toFixed(2) : null;
+  }
+
+  function swapCurrencies() {
+    const temp = baseCurrency;
+    baseCurrency = targetCurrency;
+    targetCurrency = temp;
+    fetchData();
+  }
+
+  $: dispatch("conversion", {
+    baseCurrency,
+    targetCurrency,
+    baseAmount,
+    targetAmount,
+  });
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <h1>Конвертер</h1>
+
+  <div class="boxValues">
+    <select
+      class="baseCurrency"
+      id="baseCurrency"
+      bind:value={baseCurrency}
+      on:change={handleBaseCurrencyChange}
+    >
+      {#each currencies as currency}
+        <option value={currency}>{currency}</option>
+      {/each}
+    </select>
+    <select
+      class="targetCurrency"
+      id="targetCurrency"
+      bind:value={targetCurrency}
+      on:change={handleTargetCurrencyChange}
+    >
+      {#each currencies as currency}
+        <option value={currency}>{currency}</option>
+      {/each}
+    </select>
+
+    <input
+      type="number"
+      id="baseAmount"
+      value={baseAmount}
+      on:input={handleBaseAmountChange}
+    />
+
+    {#if isLoading}
+      <input type="text" id="targetAmount" value={"Загрузка..."} disabled />
+    {:else}
+      <input
+        type="number"
+        id="targetAmount"
+        value={targetAmount}
+        on:input={handleTargetAmountChange}
+      />
+    {/if}
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <svg
+    class="arrows"
+    on:click={swapCurrencies}
+    version="1.0"
+    xmlns="http://www.w3.org/2000/svg"
+    width="100.000000pt"
+    height="100.000000pt"
+    viewBox="0 0 100.000000 100.000000"
+    preserveAspectRatio="xMidYMid meet"
+  >
+    <g transform="scale(0.100000,0.100000)" fill="#353743 " stroke="none">
+      <path
+        class="arrow-path"
+        d="M722 988 c-25 -25 -11 -54 53 -118 l66 -65 -412 -5 c-378 -5 -413 -6 -423 -22 -8 -13 -8 -23 0 -35 10 -17 45 -18 423 -23 l412 -5 -66 -65 c-66 -65 -79 -96 -50 -120 8 -6 22 -9 32 -6 10 3 70 58 133 121 l115 115 -121 120 c-66 66 -127 120 -135 120 -8 0 -20 -5 -27 -12z"
+      />
+      <path
+        class="arrow-path"
+        d="M118 362 l-120 -118 121 -122 c67 -67 127 -122 134 -122 19 0 37 21 37 45 0 12 -27 47 -65 85 l-66 65 412 5 c378 5 413 6 423 23 8 12 8 22 0 35 -10 16 -45 17 -423 22 l-412 5 66 65 c64 64 78 93 53 118 -7 7 -18 12 -26 12 -8 0 -68 -53 -134 -118z"
+      />
+    </g>
+  </svg>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
